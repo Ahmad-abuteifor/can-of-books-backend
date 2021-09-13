8332a1d7ba0f5922852f3188496cd64fd2339a68
amani-alnobani <amanialnobani@gmail.com>
2021-09-13 15:44:34 +0300

    backEnd class12 done

--- controller/book.controler.js
+++ controller/book.controler.js
@@ -5,13 +5,36 @@ const BookModel = require('../models/book.model')
 
 const getBooks = (request, response) => {
 
-    BookModel.find((error, bookstuff) => {
+  BookModel.find((error, bookstuff) => {
     response.json(bookstuff)
   });
 
 };
+const createBook = (request, response) => {
+  const { title, description, status, email } = request.body;
+console.log("hello");
 
+  const newBook = new BookModel({
+    title, description, status, email
+  });
 
+  newBook.save();
+
+  response.json(newBook);
+
+}
+const deleteBook = (request, response) => {
+  console.log("print the parms",request.params);
+  const bookId = request.params.books_id;
+ 
+  BookModel.deleteOne({ _id: bookId }, (error, deletedData) => {
+
+    response.json(deletedData);
+  });
+
+}
 module.exports = {
-    getBooks
+  getBooks,
+  createBook,
+  deleteBook
 }
--- server.js
+++ server.js
@@ -4,24 +4,24 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 app.use(cors());
-
+app.use(express.json());
 
 
 const PORT = process.env.PORT;
 const MONGO_URL = process.env.MONGO_URL;
 const DB_NAME = process.env.DB_NAME;
 
-
-
 mongoose.connect(`${MONGO_URL}/${DB_NAME}`);
-const { getBooks } = require('./controller/book.controler');
 
-
+const { getBooks,createBook,
+  deleteBook } = require('./controller/book.controler');
 
 const SeedBook = require('./helper/book.seed');
 // SeedBook()
-app.get('/books', getBooks)
 
+app.get('/books', getBooks)
+app.post('/books', createBook);
+app.delete('/books/:books_id', deleteBook);
 
 
 app.listen(PORT, () => {
